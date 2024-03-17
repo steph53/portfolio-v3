21b3f3d49fae5c5a26bf884d894499eb6b005b64
Stephane Wiston <89261662+steph53@users.noreply.github.com>
2024-03-17 19:03:06 +0100

    portfolio version 2

--- assets/js/cdemo.js
+++ assets/js/cdemo.js
@@ -106,12 +106,6 @@ THREE.CanvasRenderer = function (parameters) {
         _normal = new THREE.Vector3(),
         _normalViewMatrix = new THREE.Matrix3();
 
-    /* TODO
-    _canvas.mozImageSmoothingEnabled = false;
-    _canvas.webkitImageSmoothingEnabled = false;
-    _canvas.msImageSmoothingEnabled = false;
-    _canvas.imageSmoothingEnabled = false;
-    */
 
     // dash+gap fallbacks for Firefox and everything else
 
